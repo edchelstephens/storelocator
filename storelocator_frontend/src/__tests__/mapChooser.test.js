11314fd34cd1b598b58f56570d8602da017d83dd
Edchel Stephen <edchelstephens@gmail.com>
2022-02-14 22:20:51 +0800

    test different locations.

--- storelocator_frontend/src/__tests__/mapChooser.test.js
+++ storelocator_frontend/src/__tests__/mapChooser.test.js
@@ -1,9 +1,15 @@
 import mapChooser from "../mapChooser";
 
 describe("mapChooser", () => {
-  it("returns an image file name based on input given to it", () => {
+  it("returns an portland.jpg when portland is passed to it", () => {
     let expected = "portland.jpg";
     let actual = mapChooser("portland");
+    expect(actual).toEqual(expected);
+  });
+
+  it("returns astoria.jpg when astoria is passed to it", () => {
+    let expected = "astoria.jpg";
+    let actual = mapChooser("astoria");
     expect(actual).toEqual(expected);
   });
 });
