437a1279882f85e1cabcca490a3c67f314611f25
Edchel Stephen <edchelstephens@gmail.com>
2022-02-14 21:33:22 +0800

    update test and file.

--- storelocator_frontend/src/App.js
+++ storelocator_frontend/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Dodot Reacts Again! :)
+          Dodot Reacts Again!
         </a>
       </header>
     </div>
