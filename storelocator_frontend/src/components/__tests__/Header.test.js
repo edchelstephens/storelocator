70f091fddeb4cf6145775048e53fa699a59d29cd
Edchel Stephen <edchelstephens@gmail.com>
2022-02-14 22:00:55 +0800

    fix tests.

--- storelocator_frontend/src/components/__tests__/Header.test.js
+++ storelocator_frontend/src/components/__tests__/Header.test.js
@@ -1,5 +1,5 @@
 import { render } from "@testing-library/react";
-import Header from "./Header";
+import Header from "../Header";
 
 it("renders without crashing", () => {
   const div = document.createElement("div");
