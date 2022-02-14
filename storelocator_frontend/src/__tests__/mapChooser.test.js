d457c1f2f82542dce0822ff3d45206ae8e65c736
Edchel Stephen <edchelstephens@gmail.com>
2022-02-14 22:25:02 +0800

    default return when none is passed.

--- storelocator_frontend/src/__tests__/mapChooser.test.js
+++ storelocator_frontend/src/__tests__/mapChooser.test.js
@@ -1,15 +1,15 @@
 import mapChooser from "../mapChooser";
 
 describe("mapChooser", () => {
-  it("returns an portland.jpg when portland is passed to it", () => {
+  it("returns an image based on the location name passed to it", () => {
     let expected = "portland.jpg";
     let actual = mapChooser("portland");
     expect(actual).toEqual(expected);
   });
 
-  it("returns astoria.jpg when astoria is passed to it", () => {
-    let expected = "astoria.jpg";
-    let actual = mapChooser("astoria");
+  it("returns default.jpg when no location is passed to it", () => {
+    let expected = "default.jpg";
+    let actual = mapChooser();
     expect(actual).toEqual(expected);
   });
 });
--- storelocator_frontend/src/mapChooser.js
+++ storelocator_frontend/src/mapChooser.js
@@ -1,5 +1,8 @@
 function mapChooser(locationName) {
+  if (locationName === undefined) {
+    locationName = "default";
+  }
+
   return `${locationName}.jpg`;
 }
-
 export default mapChooser;
