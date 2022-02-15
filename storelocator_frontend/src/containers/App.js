ae06ce881e547bbe45d8e330e4adadbacb670702
Edchel Stephen <edchelstephens@gmail.com>
2022-02-15 08:44:39 +0800

    update app to render storelocator.

--- storelocator_frontend/src/containers/App.js
+++ storelocator_frontend/src/containers/App.js
@@ -1,10 +1,12 @@
 import "../App.css";
 import Header from "../components/Header";
+import StoreLocator from "./StoreLocator";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <StoreLocator />
     </div>
   );
 }
--- storelocator_frontend/src/containers/__tests__/App.test.js
+++ storelocator_frontend/src/containers/__tests__/App.test.js
@@ -1,6 +1,14 @@
-import { render } from "@testing-library/react";
+import { shallow } from "enzyme";
 import App from "../App";
 
-test("renders without crashing", () => {
-  render(<App />);
+describe("App", () => {
+  it("renders without crashing", () => {
+    let mountedApp = shallow(<App />);
+  });
+
+  it("renders StoreLocator", () => {
+    let mountedApp = shallow(<App />);
+    const locators = mountedApp.find("StoreLocator");
+    expect(locators.length).toBe(1);
+  });
 });
