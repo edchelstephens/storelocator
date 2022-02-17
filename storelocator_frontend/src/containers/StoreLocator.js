c59d4809f79396c2c7f745c3604ee4cab7ac7202
Edchel Stephen <edchelstephens@gmail.com>
2022-02-18 06:08:48 +0800

    use storeButtons.

--- storelocator_frontend/src/containers/StoreLocator.js
+++ storelocator_frontend/src/containers/StoreLocator.js
@@ -3,15 +3,29 @@ import Button from "../components/Button";
 import Map from "../components/Map";
 
 const StoreLocator = () => {
+  const shops = [
+    {
+      location: "Portland",
+      address: "123 Portland Dr",
+    },
+    {
+      location: "Astoria",
+      address: "123 Astoria Dr",
+    },
+    {
+      location: "",
+      address: "",
+    },
+  ];
+
+  const storeButtons = shops.map((shop) => {
+    return <Button location={shop.location} key={shop.address} />;
+  });
+
   return (
     <div>
       <Header />
-      <div>
-        {" "}
-        <Button location="Portland" />
-        <Button location="Astoria" />
-        <Button location="" />
-      </div>
+      <div>{storeButtons}</div>
 
       <Map />
     </div>
