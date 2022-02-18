5fbd88d853c065089e1a7aa092534d9ad481e6ff
Edchel Stephen <edchelstephens@gmail.com>
2022-02-18 08:12:04 +0800

    update mapChooser test expected filenames.

--- storelocator_frontend/src/__tests__/mapChooser.test.js
+++ storelocator_frontend/src/__tests__/mapChooser.test.js
@@ -2,13 +2,13 @@ import mapChooser from "../mapChooser";
 
 describe("mapChooser", () => {
   it("returns an image based on the location name passed to it", () => {
-    let expected = "portland.jpg";
+    let expected = "portland.png";
     let actual = mapChooser("portland");
     expect(actual).toEqual(expected);
   });
 
-  it("returns default.jpg when no location is passed to it", () => {
-    let expected = "default.jpg";
+  it("returns none.jpg when no location is passed to it", () => {
+    let expected = "none.png";
     let actual = mapChooser("");
     expect(actual).toEqual(expected);
   });
