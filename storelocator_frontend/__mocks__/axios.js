518d32565507238a5c9685754ee093c132fa1ac5
Edchel Stephen <edchelstephens@gmail.com>
2022-02-19 06:22:42 +0800

    axiosMock.

---
+++ storelocator_frontend/__mocks__/axios.js
@@ -0,0 +1,32 @@
+const axiosMock = jest.genMockFromModule("axios");
+
+let mockResponse = {
+  data: {
+    shops: [
+      {
+        location: "Portland",
+        address: "123 Portland Dr",
+      },
+      {
+        location: "Astoria",
+        address: "123 Astoria Dr",
+      },
+      {
+        location: "",
+        address: "",
+      },
+    ],
+  },
+};
+
+axiosMock.get.mockImplementation(req);
+
+function req() {
+  return new Promise(function (resolve) {
+    axiosMock.delayTimer = setTimeout(function () {
+      resolve(mockResponse);
+    }, 1000);
+  });
+}
+
+module.exports = axiosMock;
