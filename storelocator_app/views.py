2876b0ace6443efad781d3040c6f35337a63dd05
Edchel Stephen <edchelstephens@gmail.com>
2022-02-21 10:00:00 +0800

    add views to get shops.

--- storelocator_app/views.py
+++ storelocator_app/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from urllib import response
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+
+class ShopImagesAPIView(APIView):
+    """Shops api view"""
+
+    def get(self, *args, **kwargs):
+        """Return list of shop images with location."""
+        try:
+            shops = [
+                {
+                    "location": "Portland",
+                    "address": "123 Portland Dr.",
+                },
+                {
+                    "location": "Astoria",
+                    "address": "123 Astoria Dr.",
+                },
+                {
+                    "location": "",
+                    "address": "",
+                },
+            ]
+
+            response = {"shops": shops}
+
+            return Response(response, status=200)
+
+        except Exception as exc:
+            return Response("Unable to load images", status=500)
